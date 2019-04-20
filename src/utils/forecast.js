5e373fdeb693095dc042ca31b0991e554aeb5428
unknown <1starwarrior516@gmail.com>
2019-04-20 16:10:11 +0530

    fixed again

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/d12449122f92e0f947191384b22e8fa8/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
